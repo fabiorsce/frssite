bb069de9f10bcae2c093b8d136779315b2a72dc3
Fabio Santiago <fabiors_ce@yahoo.com.br>
2016-11-28 14:30:33 -0300

    Random questions

--- qa/views.py
+++ qa/views.py
@@ -6,15 +6,31 @@ Created on 24 de nov de 2016
 import random
 from .models import Question
 from django.shortcuts import render
+from qa.models import Alternative
 
 def random_test(request, number_of_questions=5):
+
+    test_questions = []
+    selected_alternatives = []
     
     if request.method == 'POST':
-        pass    
+        right_questions = 0
+        i = 0
+        while i < number_of_questions:
+            i = i + 1
+            parameter = ''.join(('question', i.__str__()))
+            alternative_id = request.POST.get(parameter, '')
+            if alternative_id:
+                alternative = Alternative.objects.get(id=int(alternative_id))
+                if alternative.is_answer:
+                    right_questions = right_questions + 1
+            test_questions.append(alternative.question)
+            selected_alternatives.append(alternative.id)
+                
+        return render(request, template_name='random_test.html', context={'questions':test_questions, 'selected_alternatives': selected_alternatives})        
     else:
         questions = Question.objects.all()
         count = questions.count()
-        test_questions = []
         i = number_of_questions
         while i > 0:
             test_questions.append(questions[random.randrange(count)])
