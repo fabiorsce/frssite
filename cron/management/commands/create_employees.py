9db26ad5176f7ab1f81b8bab5f91a37876083b5f
Fabio Santiago <fabiors_ce@yahoo.com.br>
2017-04-04 19:31:20 -0300

    Start dynamic filters

---
+++ cron/management/commands/create_employees.py
@@ -0,0 +1,41 @@
+'''
+Created on 31/12/2014
+
+@author: fabio
+'''
+
+from django.core.management.base import BaseCommand, CommandError
+from datetime import timedelta, datetime, date
+from dateutil.relativedelta import relativedelta
+from df.models import Employee
+import random
+import decimal
+
+
+
+class Command(BaseCommand):
+    
+    help = 'Create new employees in database'
+
+    def handle(self, *args, **options):
+        
+        names = ["Gaylord","Rochell","Jolyn","Janine","Laurence","Tenesha","Honey","Norma","Gretchen","Yvone","Reggie",
+                 "Wilfred","Camie","Sheldon","Tesha","Leoma","Loris","Ellie","Ha","Karen","Angle","Vernie","Cortez",
+                 "Yuonne","Stacey","Jenae","Layla","Amiee","Vinnie","Millard","Susana","Marth","Tona","Shawnna",
+                 "Rosendo","Isabella","Judy","Yahaira","Yessenia","Lori","Milissa","Belinda","Ernestina","Waldo",
+                 "Deirdre","Adrianna","Dolly","Jannet","Deanne", "Fabio"]
+
+        for i in range(1,100):
+            new_employee = Employee()
+            new_employee.first_name = names[random.randint(0,49)]
+            new_employee.last_name = names[random.randint(0,49)]
+            new_employee.birth_date = date(1970,1,1) + timedelta(days=random.randint(1,10000))
+            new_employee.role = Employee.ROLE_CHOICES[random.randint(0,2)][0]
+            new_employee.salary = decimal.Decimal(random.randrange(1000000))/100
+            new_employee.save()
+
+
+        
+
+        
+                
