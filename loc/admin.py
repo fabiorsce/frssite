5d5002ea96cc474781237650de817138653e3589
Fabio Santiago <fabiors_ce@yahoo.com.br>
2017-05-24 16:34:13 -0300

    loc

--- loc/admin.py
+++ loc/admin.py
@@ -1,3 +1,18 @@
-from django.contrib import admin
+'''
+Created on 24 de mai de 2017
 
-# Register your models here.
+@author: fabio
+'''
+
+from django.contrib import admin
+from .models import Material, Review
+
+
+@admin.register(Material)
+class MaterialAdmin(admin.ModelAdmin):
+    list_display=['serial_number', 'description', 'photo', 'price', 'lat', 'lng' ]
+    
+
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+    list_display=['material', 'user', 'message', 'created', 'status' ]
--- loc/models.py
+++ loc/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from decimal import Decimal
+from django.utils.translation import ugettext as _
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Material(models.Model):
+
+    serial_number = models.CharField(max_length=20, null=False, blank=False, unique=True)
+    description = models.CharField(max_length=200, null=False, blank=False)
+    photo = models.ImageField(null=False, blank=False, upload_to='material_images')
+    price = models.DecimalField(max_digits=10, decimal_places=2, null=False, blank=False)
+    lat = models.DecimalField(_('Latitude'), max_digits=10, decimal_places=8)
+    lng = models.DecimalField(_('Longitude'), max_digits=10, decimal_places=8)
+
+    def __str__(self):
+        return self.serial_number
+
+class ReviewStatus(models.Model):
+    description = models.CharField(max_length=20, null=False, blank=False, unique=True)
+
+    def __str__(self):
+        return self.description
+
+class Review(models.Model):
+
+    user = models.ForeignKey(User, null=False)
+    material = models.ForeignKey(Material, null=False)
+    message = models.CharField(max_length=200, null=False, blank=False)
+    created = models.DateTimeField(null=False, blank=False, auto_now_add=True)
+    lat = models.DecimalField(_('Latitude'), max_digits=10, decimal_places=8)
+    lng = models.DecimalField(_('Longitude'), max_digits=10, decimal_places=8)
+    status = models.ForeignKey(ReviewStatus, null=False)
