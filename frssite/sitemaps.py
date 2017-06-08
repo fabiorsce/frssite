57dfab9730b395d83ab755a11215e82e496931fe
Fabio Santiago <fabiors_ce@yahoo.com.br>
2017-06-08 15:36:19 -0300

    change sitemap

--- frssite/sitemaps.py
+++ frssite/sitemaps.py
@@ -3,10 +3,10 @@ from django.urls import reverse
 
 class StaticViewSitemap(sitemaps.Sitemap):
     priority = 0.5
-    changefreq = 'daily'
+    changefreq = 'yearly'
 
     def items(self):
-        return ['food_request', 'food_request_kitchen_view', 'food_request_cashier_view']
+        return ['food_request', 'food_request_kitchen_view', 'food_request_cashier_view', 'get_location', 'random_test', 'employee-list']
 
     def location(self, item):
         return reverse(item)
